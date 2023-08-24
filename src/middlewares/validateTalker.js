a854153c6e9628a1af800f825a764648aa61237f
Marcos Pimentel <mpc_marcos@hotmail.com>
2023-08-24 02:49:59 -0300

    feat: requisito 9

--- src/middlewares/validateTalker.js
+++ src/middlewares/validateTalker.js
@@ -61,12 +61,10 @@ const validateRate = (req, res, next) => {
 
 const validateRateParam = (req, res, next) => {
   const { rate } = req.query;
-  if (!rate) {
-    next();
-  }
-  if (rate < 1 || rate > 5 || !Number.isInteger(rate)) {
-    return res.status(400)
-      .json({ message: 'O campo "rate" deve ser um número inteiro entre 1 e 5' });
+  if (rate && (!Number.isInteger(Number(rate)) || Number(rate) < 1 || Number(rate) > 5)) {
+    return res.status(400).json({
+      message: 'O campo "rate" deve ser um número inteiro entre 1 e 5',
+    });
   }
   next();
 };    
